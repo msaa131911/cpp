92fb878dc1dfb47e08aa380893437d67a3780cb2
MSAA <msaa131911@gmail.com>
2025-09-19 09:35:42 +0600

    Add files via upload

---
+++ cpp_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[15];
+    int age,roll,reg;
+   
+    cout<<"enter your name:";
+    cin>>name;
+   
+    
+    
+    cout<<"enter your reg:";
+    cin>>reg;
+    
+    
+    cout<<"enter your roll:";
+    cin>>roll;
+    
+    cout<<"my name is :"<<name<<endl;
+    cout<<"my reg is :"<<reg <<endl;
+    cout<<"my roll is:"<<roll ;
+    
+
+    
+}
