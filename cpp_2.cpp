590f12718446482580dc7ef21ca5ba03ca2f1abe
MSAA <msaa131911@gmail.com>
2025-09-20 23:23:49 +0600

    Add files via upload

---
+++ cpp_2.cpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter your frist num:";
+    cin>>num1;
+    cout<<"enter your2nd number:";
+    cin>>num2;
+    
+    int sum1=num1+num2;
+    cout<<"resule:"<<sum1<<endl;
+    
+    int sum2=num1-num2;
+    cout<<"resule:"<<sum2<<endl;
+    
+    int sum3=num1*num2;
+    cout<<"resule:"<<sum3<<endl;
+    
+    double sum4=(float)num1/num2;
+    cout<<"resule:"<<sum4<<endl;
+    
+    int sum5=num1%num2;
+    cout<<"resule:"<<sum5;
+}
+
+ 
+ 
+ 
+
+    
