c72602fd4a1211ba48f2b640dc7882b2f9bcc70b
MSAA <msaa131911@gmail.com>
2025-09-25 00:07:10 +0600

    Add files via upload

---
+++ 3.cpp.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                               C++ 
+
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[10],age[5];
+    int reg,roll,sum,num1,num2;
+    
+    cout<<"enter your name:";
+    cin>>name;
+    
+    cout<<"enter your age:";
+    cin>>age;
+    
+    cout<<"enter your reg:";
+    cin>>reg;
+    
+    cout<<"enter your roll:";
+    cin>>roll;
+    
+    cout<<"enter your sum num:";
+    cin>>num1>>num2;
+    sum=num1+num2;
+    
+    //all output
+    
+    cout<<"name:"<<name<<endl;
+    cout<<"age:"<<age<<endl;
+    cout<<"reg:"<<reg<<endl;
+    cout<<"roll:"<<roll<<endl;
+    cout<<"sum:"<<sum;
+    
+    
+    
+
+    
+}
