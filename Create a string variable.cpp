179bda37f21f5304f9a3c92c7ba019e256e83a2a
MSAA <msaa131911@gmail.com>
2025-10-06 21:07:36 +0600

    Add files via upload

---
+++ Create a string variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+// Create a string variable
+    string greeting = "Hello";
+
+// Output string value
+    cout << greeting;  
+}
