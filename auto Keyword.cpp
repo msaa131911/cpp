d3b8209a263970fda43a53cdf8650502fb76787f
MSAA <msaa131911@gmail.com>
2025-10-06 21:20:41 +0600

    Add files via upload

---
+++ auto Keyword.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    auto x = 10;        // int
+    auto y = 3.14;      // double
+    auto s = "Hello";   // const char*
+    
+    cout << x << " " << y << " " << s;
+    return 0;
+}
