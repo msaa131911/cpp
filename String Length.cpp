8ef368ead1cf877db2f945af0ecfc149e77aa7e3
MSAA <msaa131911@gmail.com>
2025-10-11 19:30:24 +0600

    Add files via upload

---
+++ String Length.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    //len ber kora
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length()<<endl;
+    
+    //sm
+    string txts = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.size();
+    
+    
+}
