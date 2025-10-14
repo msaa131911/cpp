3a348934d37fc1380d20c340b6ba2a39cd3ad375
MSAA <msaa131911@gmail.com>
2025-10-14 18:27:38 +0600

    Add files via upload

---
+++ cmath.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+   cout << max(5, 10)<<endl;
+   cout << sqrt(64)<<endl;
+   cout << round(2.6)<<endl;
+   cout << log(2)<<endl;
+    
+    
+}
