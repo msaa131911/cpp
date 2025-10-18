790eedbd8934ebce666f54a960643e4302d60026
MSAA <msaa131911@gmail.com>
2025-10-18 19:09:42 +0600

    Add files via upload

---
+++ else if use.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout<<"enter your name:";
+  getline(cin,name);
+  
+  if (name=="alif"){
+      cout<<"alif is good";
+  }else if(name=="abid"){
+      cout<<"abid is a good";
+  }else{
+      cout<<"not mase";
+  }
+      
+  
+}
+   
